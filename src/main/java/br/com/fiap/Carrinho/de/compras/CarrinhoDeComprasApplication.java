07e01e5264e3e008fe9988aab1eba9b035070ea8
Vinícius Yoda <vyoda4604@gmail.com>
2023-03-07 08:33:49 -0300

    inicio

---
+++ src/main/java/br/com/fiap/Carrinho/de/compras/CarrinhoDeComprasApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.Carrinho.de.compras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrinhoDeComprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrinhoDeComprasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/Carrinho/de/compras/CarrinhoDeComprasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.Carrinho.de.compras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrinhoDeComprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

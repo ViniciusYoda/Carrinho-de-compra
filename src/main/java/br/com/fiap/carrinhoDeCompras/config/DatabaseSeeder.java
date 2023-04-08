9a667a7e128d439830accf566ca936a2d8f0aa66
Vinícius Yoda <vyoda4604@gmail.com>
2023-04-08 14:03:23 -0300

    Create DatabaseSeeder

---
+++ src/main/java/br/com/fiap/carrinhoDeCompras/config/DatabaseSeeder.java
@@ -0,0 +1,7 @@
+package br.com.fiap.carrinhoDeCompras.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import br.com.

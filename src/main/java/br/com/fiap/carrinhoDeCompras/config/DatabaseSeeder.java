bf557ec70e12fbc0586d13a0a40b5da792369bb9
Vinícius Yoda <vyoda4604@gmail.com>
2023-04-12 08:55:06 -0300

    Lidando com erros

--- src/main/java/br/com/fiap/carrinhoDeCompras/config/DatabaseSeeder.java
+++ src/main/java/br/com/fiap/carrinhoDeCompras/config/DatabaseSeeder.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import br.com.fiap.carrinhoDeCompras.models.Pagamento;
 import br.com.fiap.carrinhoDeCompras.models.Produtos;
 import br.com.fiap.carrinhoDeCompras.repositoty.ProdutosRepository;
 
@@ -18,9 +19,9 @@ public class DatabaseSeeder implements CommandLineRunner {
    @Override
    public void run(String... args) throws Exception {
       produtosRepository.saveAll(List.of(
-         new Produtos(1l, "Café", 4.75, "Alimentos"),
-         new Produtos(2l, "Chocolate", 3.30, "Alimentos"),
-         new Produtos(3l, "Água", 1.75, "Alimentos")
+         new Produtos(1l, "Café", 4.75, "Alimentos", null),
+         new Produtos(2l, "Chocolate", 3.30, "Alimentos", null),
+         new Produtos(3l, "Água", 1.75, "Alimentos", null)
       ));
    }
 }

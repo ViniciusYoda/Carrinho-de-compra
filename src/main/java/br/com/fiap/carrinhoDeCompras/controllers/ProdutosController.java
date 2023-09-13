d8aebecd85299ca8ba6852310c0c550652178f5f
Vinícius Yoda <vyoda4604@gmail.com>
2023-09-13 10:15:06 -0300

    arr

--- src/main/java/br/com/fiap/carrinhoDeCompras/controllers/ProdutosController.java
+++ src/main/java/br/com/fiap/carrinhoDeCompras/controllers/ProdutosController.java
@@ -35,7 +35,7 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping("/api/produtos")
 @Slf4j
-@SecurityRequirement(name = "bearer-key")
+// @SecurityRequirement(name = "bearer-key")
 @Tag(name = "produtos")
 public class ProdutosController {
 
